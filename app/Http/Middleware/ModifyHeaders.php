6bd956d1625a96e13bc254e17b3789ec2f39600e
Alex <kochubey.alexander94@gmail.com>
2019-12-24 09:01:54 +0200

    Fix ModifyHeadersMiddleware

--- app/Http/Middleware/ModifyHeaders.php
+++ app/Http/Middleware/ModifyHeaders.php
@@ -4,7 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class ModifyHeadersMiddleware
+class ModifyHeaders
 {
     /**
      * Handle an incoming request.
@@ -17,7 +17,8 @@ class ModifyHeadersMiddleware
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
 
         return $response;
     }
