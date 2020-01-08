7936d49d14d3ab69dcaf69bccb3a20e43b0d0d84
Alex <kochubey.alexander94@gmail.com>
2020-01-08 23:49:15 +0200

    Fixed FoodDeliveryOrganization (added base model trait)

--- app/Models/FoodDeliveryOrganization.php
+++ app/Models/FoodDeliveryOrganization.php
@@ -1,9 +1,8 @@
 <?php
 
-
 namespace App\Models;
 
-
+use App\Traits\CrudModelTrait;
 use App\Traits\JWTSubjectTrait;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -14,8 +13,15 @@ class FoodDeliveryOrganization extends Authenticatable implements JWTSubject
 {
     use JWTSubjectTrait;
     use Notifiable;
+    use CrudModelTrait;
 
     protected $fillable = ['email', 'password'];
+
+    public function __construct(array $attributes = [])
+    {
+        $this->initCrud();
+        parent::__construct($attributes);
+    }
 
     public function setPasswordAttribute($value)
     {
